56d5a0bed60548f8758467d9c79ab21eebae300e
Michael Pompei <m.pompei@setonhill.edu>
2020-04-29 12:01:50 -0400

    HW 7

---
+++ Pompei Homework 7/Pompei Homework 7/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Pompei Homework 7
+//
+//  Created by Michael David Pompei on 4/21/20.
+//  Copyright © 2020 Michael David Pompei. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate }
+
+
+@IBOutlet weak var DIstanceLabel: UILabel!
+
+
+let locman: CLLocationManager = CLLocationManager()
+var startlocation: CLLocation:
+let PNCLatitude: CLLocationDegrees = 40.4469
+let PNCLongitude: CLLocationDegrees = 80.0057
+
+
+
+
+
+
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    locMan.delegate = self
+    locman.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+    locman.distanceFilter = 1609
+    locman.requestWhenInUseAuthorization()
+    locman.startUpdatingLocation()
+startlocation = nil
+}
+
+
+}
+
+
+
+
+
+
+
+
+
