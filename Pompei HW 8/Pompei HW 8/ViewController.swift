cced43a86f3df5ad22a0d9c485239a6fa90f40e9
Michael Pompei <m.pompei@setonhill.edu>
2020-04-29 12:29:34 -0400

    HW 8

---
+++ Pompei HW 8/Pompei HW 8/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Pompei HW 8
+//
+//  Created by Michael David Pompei on 4/29/20.
+//  Copyright © 2020 Michael David Pompei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var OutputLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
